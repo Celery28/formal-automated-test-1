63e27ff365733462e2f0d8be5d80f1d50b323d09
NuanNuan1121 <772047518@qq.com>
2018-01-23 11:15:53 +0800

    修改main函数，运行自动化解脚本
    修改main函数，运行自动化解脚本

--- main.py
+++ main.py
@@ -46,18 +46,18 @@ if __name__ == '__main__':
     """
     课程库列表页测试用例
     """
-    # # 验证课程库列表课程数量
-    # testsuite.addTest(cases.CoursesTestCase("test_courses_number"))
-    # # 验证课程库列表课程方向筛选
-    # testsuite.addTest(cases.CoursesTestCase("test_first_category"))
-    # # 验证课程库列表课程分类筛选
-    # testsuite.addTest(cases.CoursesTestCase("test_sub_category"))
-    # # 验证课程库列表筛选课程最新最热状态
-    # testsuite.addTest(cases.CoursesTestCase("test_courses_sort"))
-    # # 验证课程库收费与免费过滤器
-    # testsuite.addTest((cases.CoursesTestCase("test_courses_price_filter")))
-    # # 验证课程库列表筛选课程难度
-    # testsuite.addTest(cases.CoursesTestCase("test_course_difficulty_level"))
+    # 验证课程库列表课程数量
+    testsuite.addTest(cases.CoursesTestCase("test_courses_number"))
+    # 验证课程库列表课程方向筛选
+    testsuite.addTest(cases.CoursesTestCase("test_first_category"))
+    # 验证课程库列表课程分类筛选
+    testsuite.addTest(cases.CoursesTestCase("test_sub_category"))
+    # 验证课程库列表筛选课程最新最热状态
+    testsuite.addTest(cases.CoursesTestCase("test_courses_sort"))
+    # 验证课程库收费与免费过滤器
+    testsuite.addTest((cases.CoursesTestCase("test_courses_price_filter")))
+    # 验证课程库列表筛选课程难度
+    testsuite.addTest(cases.CoursesTestCase("test_course_difficulty_level"))
 
     """
     李伟用例
@@ -72,26 +72,26 @@ if __name__ == '__main__':
     """
     课程详情页测试用例
     """
-    # # 验证免费课程购买
-    # testsuite.addTest(cases.CourseTestCase('test_free_course_buy'))
-    # # 验证收费课程购买
-    # testsuite.addTest(cases.CourseTestCase('test_no_free_course_buy'))
-    # # 验证课程关注
-    # testsuite.addTest(cases.CourseTestCase('test_course_favorite'))
-    # # 验证课程标签
-    # testsuite.addTest(cases.CourseTestCase('test_course_tag'))
-    # # 验证教师关注
-    # testsuite.addTest(cases.CourseTestCase('test_teacher_favorite'))
-    # # 验证教师点赞
-    # testsuite.addTest(cases.CourseTestCase('test_teacher_vote'))
-    # # 验证打开教师详情页
-    # testsuite.addTest(cases.CourseTestCase('test_open_teacher'))
+    # 验证免费课程购买
+    testsuite.addTest(cases.CourseTestCase('test_free_course_buy'))
+    # 验证收费课程购买
+    testsuite.addTest(cases.CourseTestCase('test_no_free_course_buy'))
+    # 验证课程关注
+    testsuite.addTest(cases.CourseTestCase('test_course_favorite'))
+    # 验证课程标签
+    testsuite.addTest(cases.CourseTestCase('test_course_tag'))
+    # 验证教师关注
+    testsuite.addTest(cases.CourseTestCase('test_teacher_favorite'))
+    # 验证教师点赞
+    testsuite.addTest(cases.CourseTestCase('test_teacher_vote'))
+    # 验证打开教师详情页
+    testsuite.addTest(cases.CourseTestCase('test_open_teacher'))
 
     """
     岗位列表页测试用例
     """
-    # # 证进入岗位课详情页
-    # testsuite.addTest(cases.PostCoursesTestCase("test_post_course"))
+    # 证进入岗位课详情页
+    testsuite.addTest(cases.PostCoursesTestCase("test_post_course"))
 
     """
     岗位课详情页测试用例
@@ -103,58 +103,58 @@ if __name__ == '__main__':
     """
     教师列表页测试用例
     """
-    # # 验证随机筛选教师方向
-    # testsuite.addTest(cases.TeachersTestCase("test_teachers_category"))
-    # # 随机进入教师详情页
-    # testsuite.addTest(cases.TeachersTestCase("test_teachers_page"))
+    # 验证随机筛选教师方向
+    testsuite.addTest(cases.TeachersTestCase("test_teachers_category"))
+    # 随机进入教师详情页
+    testsuite.addTest(cases.TeachersTestCase("test_teachers_page"))
 
     """
     教师详情页测试用例
     """
-    # # 验证教师详情页关注功能
-    # testsuite.addTest(cases.TeacherTestCase("test_teacher_details_favorite"))
-    # # 验证教师点赞功能
-    # testsuite.addTest(cases.TeacherTestCase("test_teacher_zan"))
-    # # 验证随机进入同方向讲师
-    # testsuite.addTest(cases.TeacherTestCase("test_same_direction_teacher"))
-    # # 验证随机进入金牌讲师页面
-    # testsuite.addTest(cases.TeacherTestCase("test_gold_medal_teacher"))
+    # 验证教师详情页关注功能
+    testsuite.addTest(cases.TeacherTestCase("test_teacher_details_favorite"))
+    # 验证教师点赞功能
+    testsuite.addTest(cases.TeacherTestCase("test_teacher_zan"))
+    # 验证随机进入同方向讲师
+    testsuite.addTest(cases.TeacherTestCase("test_same_direction_teacher"))
+    # 验证随机进入金牌讲师页面
+    testsuite.addTest(cases.TeacherTestCase("test_gold_medal_teacher"))
 
     """
     搜索页面用例
     """
-    # # 验证搜索课程\帖子\老师\学友是否有数据
-    # testsuite.addTest(cases.SearchTestCase("test_search_course"))
-    # testsuite.addTest(cases.SearchTestCase("test_search_post"))
-    # testsuite.addTest(cases.SearchTestCase("test_search_teacher"))
-    # testsuite.addTest(cases.SearchTestCase("test_search_student"))
-    # # 验证搜索课程随机进入课程详情页功能
-    # testsuite.addTest(cases.SearchTestCase("test_search_course_enter_course_details_page"))
-    # # 验证随机搜索课程随机进入课程标签页
-    # testsuite.addTest(cases.SearchTestCase("test_search_course_enter_label_details_page"))
+    # 验证搜索课程\帖子\老师\学友是否有数据
+    testsuite.addTest(cases.SearchTestCase("test_search_course"))
+    testsuite.addTest(cases.SearchTestCase("test_search_post"))
+    testsuite.addTest(cases.SearchTestCase("test_search_teacher"))
+    testsuite.addTest(cases.SearchTestCase("test_search_student"))
+    # 验证搜索课程随机进入课程详情页功能
+    testsuite.addTest(cases.SearchTestCase("test_search_course_enter_course_details_page"))
+    # 验证随机搜索课程随机进入课程标签页
+    testsuite.addTest(cases.SearchTestCase("test_search_course_enter_label_details_page"))
     # TODO: 这个用例有些问题，需要优化
-    # # 验证搜索课程翻页功能
-    # # testsuite.addTest(cases.SearchTestCase("test_search_course_flip_pages"))
-    # # 验证搜索帖子随机进入帖子详情页
-    # testsuite.addTest(cases.SearchTestCase("test_search_post_enter_post_details_page"))
-    # # 验证搜索帖子随机进入板块详情页
-    # testsuite.addTest(cases.SearchTestCase("test_search_post_enter_plate_details_page"))
-    # # 验证搜索帖子进入个人主页
-    # testsuite.addTest(cases.SearchTestCase("test_search_post_enter_homepage"))
-    # # 验证搜索教师随机进入教师主页
-    # testsuite.addTest(cases.SearchTestCase("test_search_teacher_enter_teacher_homepage"))
-    # # 验证搜索学友随机进入个人主页
-    # testsuite.addTest(cases.SearchTestCase("test_search_user_enter_homepage"))
-    # 验证搜索学友随机加关注按钮
-    # testsuite.addTest(cases.SearchTestCase(""))
+    # 验证搜索课程翻页功能
+    # testsuite.addTest(cases.SearchTestCase("test_search_course_flip_pages"))
+    # 验证搜索帖子随机进入帖子详情页
+    testsuite.addTest(cases.SearchTestCase("test_search_post_enter_post_details_page"))
+    # 验证搜索帖子随机进入板块详情页
+    testsuite.addTest(cases.SearchTestCase("test_search_post_enter_plate_details_page"))
+    # 验证搜索帖子进入个人主页
+    testsuite.addTest(cases.SearchTestCase("test_search_post_enter_homepage"))
+    # 验证搜索教师随机进入教师主页
+    testsuite.addTest(cases.SearchTestCase("test_search_teacher_enter_teacher_homepage"))
+    # 验证搜索学友随机进入个人主页
+    testsuite.addTest(cases.SearchTestCase("test_search_user_enter_homepage"))
+    验证搜索学友随机加关注按钮
+    testsuite.addTest(cases.SearchTestCase(""))
 
     """
     系列课页面用例
     """
-    # # 测试系列课程数量
-    # testsuite.addTest(cases.SeriesCourseTestCase("test_series_course_number"))
-    # # 测试系列课随机进入某节课程
-    # testsuite.addTest(cases.SeriesCourseTestCase("test_series_course"))
+    # 测试系列课程数量
+    testsuite.addTest(cases.SeriesCourseTestCase("test_series_course_number"))
+    # 测试系列课随机进入某节课程
+    testsuite.addTest(cases.SeriesCourseTestCase("test_series_course"))
 
     # runner = unittest.TextTestRunner()
     # runner.run(testsuite)
